c8488118b013132a9eea35a288e1e53d85c96dc6
ArcherGrey <498792539@qq.com>
2021-01-25 11:11:52 +0800

    chore: change branch set

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ArcherGrey/Grimoire.git master:gh-pages
+git push -f git@github.com:ArcherGrey/Grimoire.git main:gh-pages
 
 cd -
