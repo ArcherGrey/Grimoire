04392699417a4523511aa11fb3c3f604dcea5730
ArcherGrey <498792539@qq.com>
2021-01-26 23:43:34 +0800

    docs: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   title: "Grimoire", // 标题
   description: "Magic Book",
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   themeConfig: {
     // sidebar: "auto",
@@ -15,9 +15,9 @@ module.exports = {
         items: [
           { text: "JavaScript", link: "/js/" },
           { text: "框架", items: [{ text: "Vue", link: "/vue/" }] },
-          { text: "算法", items: [{ text: "leetcode", link: "/leetcode/" }] }
-        ]
-      }
+        ],
+      },
+      { text: "算法", items: [{ text: "leetcode", link: "/leetcode/" }] },
     ],
     sidebarDepth: 3,
     sidebar: {
@@ -25,26 +25,26 @@ module.exports = {
         {
           title: "Vue",
           collapsable: false,
-          children: [{ title: "基础", children: ["reactivity"] }]
+          children: [{ title: "基础", children: ["reactivity"] }],
           // children: ["reactivity"]
-        }
+        },
       ],
       "/js/": [
         {
           title: "JavaScript",
           collapsable: false,
-          children: [{ title: "应用", children: ["databind"] }]
+          children: [{ title: "应用", children: ["databind"] }],
           // children: ["reactivity"]
-        }
+        },
       ],
       "/leetcode/": [
         {
           // title: "sua",
           collapsable: false,
-          children: ["", "1", "2", "3", "4"]
-        }
-      ]
-    }
+          children: ["", "1", "2", "3", "4"],
+        },
+      ],
+    },
   },
-  plugins: ["@vuepress/back-to-top", "@vuepress/nprogress"]
+  plugins: ["@vuepress/back-to-top", "@vuepress/nprogress"],
 };
